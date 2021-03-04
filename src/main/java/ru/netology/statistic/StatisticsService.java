802386375cc0c27e32a9dc107e31cba1d235006d
Anna Starkova <annastarkova2018@gmail.com>
2021-03-04 23:31:07 +0500

    added test

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -15,4 +15,37 @@ public class StatisticsService {
                 current_max = income;
         return current_max;
     }
+
+//    public long findMin(long[] incomes) {
+//        long current_min_index = 0;
+//        long current_min = incomes[0];
+//        for (long income : incomes)
+//            if (current_min > income)
+//                current_min = income;
+//        return current_min;
+//    }
+
+    public long maxFirst(long[] purchases) {
+        long max = purchases[0];
+        long index = 0;
+        for (int i = 0; i < purchases.length; i++) {
+            if (purchases[i] > max) {
+                max = purchases[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public long max(long[] purchases) {
+        long max = purchases[0];
+        long index = 0;
+        for (int i = 0; i < purchases.length; i++) {
+            if (purchases[i] >= max) {
+                max = purchases[i];
+                index = i;
+            }
+        }
+        return index;
+    }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,4 +18,41 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+//    @Test
+//    void findMin() {
+//        StatisticsService service = new StatisticsService();
+//
+//        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+//        long expected = 3;
+//
+//        long actual = service.findMin(incomesInBillions);
+//
+//        assertEquals(expected, actual);
+//    }
+
+    @org.junit.jupiter.api.Test
+    void maxFirst() {
+        StatisticsService service = new StatisticsService();
+        long[] purchases = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 0;
+
+        // вызываем целевой метод:
+        long actual = service.maxFirst(purchases);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void max() {
+        StatisticsService service = new StatisticsService();
+        long[] purchases = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 10;
+
+        // вызываем целевой метод:
+        long actual = service.max(purchases);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
 }
