3374e78ad6d92e3dd63da6c0fdb4b2bff55e9b6a
Anna Starkova <annastarkova2018@gmail.com>
2021-03-06 17:54:36 +0500

    100%

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -4,17 +4,18 @@ public class StatisticsService {
     /**
      * Calculate index of max income
      *
-     * @param incomes - array of incomes
+     * @param purchases - array of incomes
      * @return - index of first max value
      */
-    public long findMax(long[] incomes) {
-        long current_max_index = 0;
-        long current_max = incomes[0];
-        for (long income : incomes)
-            if (current_max < income)
-                current_max = income;
-        return current_max;
-    }
+//    public long findMax(long[] incomes) {
+//        long current_max_index = 0;
+//        long current_max = incomes[0];
+//        for (long income : incomes)
+//            if (current_max < income) {
+//                current_max = income;
+//            }
+//        return current_max;
+//    }
 
 //    public long findMin(long[] incomes) {
 //        long current_min_index = 0;
@@ -25,17 +26,17 @@ public class StatisticsService {
 //        return current_min;
 //    }
 
-    public long maxFirst(long[] purchases) {
-        long max = purchases[0];
-        long index = 0;
-        for (int i = 0; i < purchases.length; i++) {
-            if (purchases[i] > max) {
-                max = purchases[i];
-                index = i;
-            }
-        }
-        return index;
-    }
+//    public long maxFirst(long[] purchases) {
+//        long max = purchases[0];
+//        long index = 0;
+//        for (int i = 0; i < purchases.length; i++) {
+//            if (purchases[i] > max) {
+//                max = purchases[i];
+//                index = i;
+//            }
+//        }
+//        return index;
+//    }
 
     public long max(long[] purchases) {
         long max = purchases[0];
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,17 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StatisticsServiceTest {
 
-    @Test
-    void findMax() {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMax(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
+//    @Test
+//    void findMax() {
+//        StatisticsService service = new StatisticsService();
+//
+//        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+//        long expected = 12;
+//
+//        long actual = service.findMax(incomesInBillions);
+//
+//        assertEquals(expected, actual);
+//    }
 
 //    @Test
 //    void findMin() {
@@ -31,18 +31,18 @@ class StatisticsServiceTest {
 //        assertEquals(expected, actual);
 //    }
 
-    @org.junit.jupiter.api.Test
-    void maxFirst() {
-        StatisticsService service = new StatisticsService();
-        long[] purchases = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 0;
-
-        // вызываем целевой метод:
-        long actual = service.maxFirst(purchases);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
+//    @org.junit.jupiter.api.Test
+//    void maxFirst() {
+//        StatisticsService service = new StatisticsService();
+//        long[] purchases = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+//        long expected = 0;
+//
+//        // вызываем целевой метод:
+//        long actual = service.maxFirst(purchases);
+//
+//        // производим проверку (сравниваем ожидаемый и фактический):
+//        assertEquals(expected, actual);
+//    }
     @org.junit.jupiter.api.Test
     void max() {
         StatisticsService service = new StatisticsService();
